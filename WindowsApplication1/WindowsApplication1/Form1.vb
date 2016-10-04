6b557dddd4ec2f397725e1b71c0da4b0b04fefa6
kristelschzrjs <marina.sanchezrjs@gmail.com>
2016-10-03 22:06:55 -0500

    prueba de la kris :V

--- WindowsApplication1/WindowsApplication1/Form1.vb
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -1,6 +1,7 @@
 ﻿Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim prueba as string = "Desde github :3"
+        Dim prueba As String = "Desde github :3"
+        Dim la_kris_se_la_come As String = "si"
     End Sub
 End Class
