81b786b2ab753cc90e2432f6fca1785d4d217de5
kristelschzrjs <marina.sanchezrjs@gmail.com>
2016-10-03 22:08:46 -0500

    prueba

--- WindowsApplication1/WindowsApplication1/Form1.vb
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -3,5 +3,6 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim prueba As String = "Desde github :3"
         Dim la_kris_se_la_come As String = "si"
+        'prueba
     End Sub
 End Class
