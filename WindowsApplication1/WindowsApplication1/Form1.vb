7a12f968b2d534a0a8b227f9bfcf37ce37601a24
desexterno1 <des_externo1@ccontrol.com.mx>
2016-10-03 22:21:46 -0500

    ok

--- WindowsApplication1/WindowsApplication1/Form1.vb
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim prueba as string = "Desde github :3"
+        Dim prueba As String = "Desde github :3"
     End Sub
 End Class
